69987e3de9c33de136f1118e561c8d5a9143af4e
demigods <a13752365641@126.com>
2023-03-09 10:20:17 +0800

    feat:添加等级配置。

---
+++ src/config/level.ts
@@ -0,0 +1,32 @@
+export default [{
+  code: 0,
+  name: 'LV.-'
+},
+{
+  code: 1,
+  name: 'LV.1'
+},
+{
+  code: 2,
+  name: 'LV.2'
+},
+{
+  code: 3,
+  name: 'LV.3'
+},
+{
+  code: 4,
+  name: 'LV.4'
+},
+{
+  code: 5,
+  name: 'LV.5'
+},
+{
+  code: 6,
+  name: 'LV.6'
+},
+{
+  code: 7,
+  name: 'LV.7'
+}]
