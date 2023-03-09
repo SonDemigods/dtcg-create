0017fad793ea48b77aee065c78894aba8258d550
demigods <a13752365641@126.com>
2023-03-09 13:17:51 +0800

    feat:添加属性配置。

---
+++ src/config/property.ts
@@ -0,0 +1,28 @@
+export default [{
+  code: 0,
+  name: '-'
+},
+{
+  code: 1,
+  name: '病毒种'
+},
+{
+  code: 2,
+  name: '数据种'
+},
+{
+  code: 3,
+  name: '疫苗种'
+},
+{
+  code: 4,
+  name: '自由'
+},
+{
+  code: 5,
+  name: '不明'
+},
+{
+  code: 6,
+  name: '可变'
+}]
