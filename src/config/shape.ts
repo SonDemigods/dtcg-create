23909f13e7e433121c3a3bb9d03259ef8f19391d
demigods <a13752365641@126.com>
2023-03-09 13:18:10 +0800

    feat:添加形态配置。

---
+++ src/config/shape.ts
@@ -0,0 +1,32 @@
+export default [{
+  code: 0,
+  name: '-'
+},
+{
+  code: 1,
+  name: '幼年期'
+},
+{
+  code: 2,
+  name: '成长期'
+},
+{
+  code: 3,
+  name: '成熟期'
+},
+{
+  code: 4,
+  name: '完全体'
+},
+{
+  code: 5,
+  name: '究极体'
+},
+{
+  code: 6,
+  name: '混合体'
+},
+{
+  code: 7,
+  name: '装甲体'
+}]
