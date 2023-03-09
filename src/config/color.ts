87a6a20e3e0acbadc63509de18e2a0403c327cf0
demigods <a13752365641@126.com>
2023-03-09 13:17:27 +0800

    feat:添加颜色配置。

---
+++ src/config/color.ts
@@ -0,0 +1,28 @@
+export default [{
+  code: 1,
+  name: '红'
+},
+{
+  code: 2,
+  name: '蓝'
+},
+{
+  code: 3,
+  name: '黄'
+},
+{
+  code: 4,
+  name: '绿'
+},
+{
+  code: 5,
+  name: '黑'
+},
+{
+  code: 6,
+  name: '紫'
+},
+{
+  code: 7,
+  name: '白'
+}]
