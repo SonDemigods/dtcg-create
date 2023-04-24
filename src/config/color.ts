6c18fb8748aa79ca2266d16fa2c54fbbfc7f671e
demigods <a13752365641@126.com>
2023-04-24 12:01:09 +0800

    feat:添加颜色值

--- src/config/color.ts
+++ src/config/color.ts
@@ -1,28 +1,35 @@
 export default [{
   code: 1,
-  name: '红'
+  name: '红',
+  value: '#d50000'
 },
 {
   code: 2,
-  name: '蓝'
+  name: '蓝',
+  value: '#189fed'
 },
 {
   code: 3,
-  name: '黄'
+  name: '黄',
+  value: '#f0ac09'
 },
 {
   code: 4,
-  name: '绿'
+  name: '绿',
+  value: '#00bd97'
 },
 {
   code: 5,
-  name: '黑'
+  name: '黑',
+  value: '#000000'
 },
 {
   code: 6,
-  name: '紫'
+  name: '紫',
+  value: '#752ae6'
 },
 {
   code: 7,
-  name: '白'
+  name: '白',
+  value: '#ffffff'
 }]
